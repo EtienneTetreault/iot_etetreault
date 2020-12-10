c38e7eb1bba24daf6962a3a74072dde07d2b58a1
EtienneTetreault <etienne.tetreault@usherbrooke>
2020-12-10 10:29:46 -0500

    Full restructure of Xstate machine for alarmclock:
    - Takes full advantage of Xstate library possibilities
    - Use of nested state : "Ringing" contains substate of various alarms
    - All alarms are lists declare outside and machine just iterate
    - Xstate internal delay (the "after" property) are use for all alarms
    - Initial "Boot" state with delay to allow NodeRed time to boot
    - No more CleanUp state : all reseting in Idle
    - New MQTT topic to fit with new AlarmClock library from ESParkle
    - Interpreter's Listener "onTransition" to publish new state to NodeRed

--- node_red_script/xstate_alarmclock_Main.js
+++ node_red_script/xstate_alarmclock_Main.js
@@ -1,10 +1,14 @@
-
 // ==== EXTRA =====
 const node = {
+    send: function (msgOut) {
+        console.log(msgOut);
+    },
     status: function (msgOut) {
         console.log(msgOut);
-    }
-};
+
+    },
+}
+// ================
 
 // ==== TEST SECTION ===
 node_xstate_clock({ topic: 'disabled', payload: false });
@@ -18,11 +22,19 @@ function node_xstate_clock(msg) {
     // alarmClockService =context.get("alarmClockService"); //to retrieve a variable 
     const alarmClockService = require('./xstate_alarmclock_Setup.js')
 
+
+    function throw_error() {
+        node.status({ text: "ERROR!" });
+        node.send({ payload: "ERROR!" });
+
+    }
+
     // parsing input!!
     switch (msg.topic) {
         case "disabled": // from the Node Red UI toggling / switch button
             if (msg.payload == false) { event_input = 'ARMING' }
-            else { event_input = alarmClockService.state.value == "armed" ? 'DISARMING' : 'STOP' }
+            else if (msg.payload == true) { event_input = 'STOP' }
+            else { throw_error() }
             break;
         case "alarmClock": // from the Node Red alarm clock / timer
             event_input = "LAUNCHING";
@@ -30,16 +42,13 @@ function node_xstate_clock(msg) {
         case "remoteScale/fromESP": // from the remote scale PCB
             event_input = "STOP"
             break;
-        case "mrdiynotifier/status": // from the alarm clock PCB
-            if (msg.payload == "idle") { event_input = "REPLAY_ALARM" }
-            break;
         default:
-            event_input = "BUG";
-            console.log('BUG!!!!');
+            throw_error();
             break;
 
     }
 
     alarmClockService.send(event_input);
-    node.status({ text: alarmClockService.state.value });
+
+    // node.status({ text: alarmClockService.state.value });
 }
